ce8499278d5964ad4708d53a6b76880a1790988a
Aakash Singh <aakash.singh@joshsoftware.com>
2023-01-25 15:10:19 +0530

    add fake user data generator

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/bxcodec/faker/v3"
+)
+
+type User struct {
+	Name           string `faker:"name"`
+	Age            uint   `faker:"boundary_start=25, boundary_end=45"`
+	Email          string `faker:"email"`
+	Phone          string `faker:"e_164_phone_number"`
+	CreditCard     string `faker:"cc_number"`
+	CreditCardType string `faker:"cc_type"`
+}
+
+// make user implement stringer
+func (u User) String() string {
+	b, err := json.MarshalIndent(u, "", "\t")
+	if err != nil {
+		log.Fatal(err)
+		return ""
+	}
+	return string(b)
+}
+
+func main() {
+	var user User
+	err := faker.FakeData(&user)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(user)
+}
